0245265647978565ddf12cc319cb833a66cc9608
bienheureuseuwineza <85676961+Aime78@users.noreply.github.com>
2023-01-05 19:27:08 +0200

    initial commit.

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["Quicksand", "arial"],
+      },
+      colors: {
+        primary: "#ff385c",
+      },
+      screens: {
+        xl: { max: "1279px" },
+        lg: { max: "1023px" },
+        md: { max: "940px" },
+        sm: { max: "640px" },
+      },
+    },
+  },
+  plugins: [],
+};
