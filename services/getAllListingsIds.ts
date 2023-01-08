cc827e693049b0d928d7e4e94fc8a8aa189352bb
esther921 <estherniyonkuru51@gmail.com>
2023-01-08 16:21:21 +0200

    fix:testing the deployment

--- services/getAllListingsIds.ts
+++ services/getAllListingsIds.ts
@@ -5,7 +5,7 @@ import { listingProp } from "types/listingInterface";
 const getAllListingsIds = async () => {
   try {
     const resp = await axios.get(
-      `${apiURL}/listings?_limit=20
+      `${apiURL}/listings?_limit=100
                 `
     );
 
